d37ad079d56679bb78720369551318a83b2adaae
Midhun Das L <95303776+Midhun-2001@users.noreply.github.com>
2023-02-08 15:53:51 +0530

    Add files via upload

---
+++ flipkart_headphone_details_preprocessing.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# Read the CSV file
+df = pd.read_csv('flipkart_product_details.csv')
+
+# Remove columns with names starting with "Unnamed"
+df.drop(df.columns[df.columns.str.startswith('Unnamed')], axis=1, inplace=True)
+
+# Drop unwanted columns
+df = df.drop(columns=['Headphone Design', 'Accessories Included', 'Inline Remote', 'Sales Package'])
+
+# Replace values in "Headphone Type" column
+df["Headphone_Type"] = df["Headphone Type"].replace({"True Wireless": "In Ear", "In the Ear": "In Ear", "On the Ear": "On Ear"})
+
+# Drop the original "Headphone Type" column
+df = df.drop(columns=["Headphone Type"])
+
+# Save the updated DataFrame to a new CSV file
+df.to_csv('flipkart_headphone_details.csv', index=False)
